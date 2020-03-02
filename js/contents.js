1aa63d378a97760cc6290765907290b21f683206
jiwonjang27 <59678412+jiwonjang27@users.noreply.github.com>
2020-03-02 16:30:47 +0900

    0302 2

--- js/contents.js
+++ js/contents.js
@@ -1,46 +1,3 @@
-function resizeMasonryItem(item){
-    /* Get the grid object, its row-gap, and the size of its implicit rows */
-    var grid = document.getElementsByClassName('masonry')[0],
-        rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap')),
-        rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
-  
-    
-    var rowSpan = Math.ceil((item.querySelector('.masonry-content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
-  
-    /* Set the spanning as calculated above (S) */
-    item.style.gridRowEnd = 'span '+rowSpan;
-  }
-  
- 
-function resizeAllMasonryItems(){
-    // Get all item class objects in one list
-    var allItems = document.getElementsByClassName('workframe');
-  
-    for(var i=0;i<allItems.length;i++){
-      resizeMasonryItem(allItems[i]);
-    }
-  }
-
-
-function waitForImages() {
-    var allItems = document.getElementsByClassName('workframe');
-    for(var i=0;i<allItems.length;i++){
-      imagesLoaded( allItems[i], function(instance) {
-        var item = instance.elements[0];
-        resizeMasonryItem(item);
-      } );
-    }
-  }
-
-  /* Resize all the grid items on the load and resize events */
-var masonryEvents = ['load', 'resize'];
-masonryEvents.forEach( function(event) {
-  window.addEventListener(event, resizeAllMasonryItems);
-} );
-  
-waitForImages();
-
-
 
 filterSelection("all")
 function filterSelection(c) {
@@ -89,3 +46,47 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+
+
+function resizeMasonryItem(item){
+    /* Get the grid object, its row-gap, and the size of its implicit rows */
+    var grid = document.getElementsByClassName('masonry')[0],
+        rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap')),
+        rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+  
+    
+    var rowSpan = Math.ceil((item.querySelector('.masonry-content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+  
+    /* Set the spanning as calculated above (S) */
+    item.style.gridRowEnd = 'span '+rowSpan;
+  }
+  
+ 
+function resizeAllMasonryItems(){
+    // Get all item class objects in one list
+    var allItems = document.getElementsByClassName('workframe');
+  
+    for(var i=0;i<allItems.length;i++){
+      resizeMasonryItem(allItems[i]);
+    }
+  }
+
+
+function waitForImages() {
+    var allItems = document.getElementsByClassName('workframe');
+    for(var i=0;i<allItems.length;i++){
+      imagesLoaded( allItems[i], function(instance) {
+        var item = instance.elements[0];
+        resizeMasonryItem(item);
+      } );
+    }
+  }
+
+  /* Resize all the grid items on the load and resize events */
+var masonryEvents = ['load', 'resize'];
+masonryEvents.forEach( function(event) {
+  window.addEventListener(event, resizeAllMasonryItems);
+} );
+  
+waitForImages();
