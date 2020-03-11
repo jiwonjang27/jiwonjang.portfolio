b8e3f053d3491cd072cf1f15975fe8a41d9f5608
jiwonjang27 <59678412+jiwonjang27@users.noreply.github.com>
2020-03-11 20:53:55 +0900

    2/3

--- js/header.js
+++ js/header.js
@@ -4,6 +4,7 @@ $(".resume").click(function(){
     $(".fifth-row").slideToggle(500, function(){})
 })
 
+// top bar sticky
 $(document).ready(function(){
     $(window).scroll(function(){
         var topBar = $(".topbar-before").offset()
@@ -16,3 +17,38 @@ $(document).ready(function(){
         }
     });
 });
+
+// top bar disappear reappear
+var lastScrollTop = 0;
+
+$(window).scroll(function () {
+
+var st = $(this).scrollTop();
+        if (st < lastScrollTop){
+            $('.topbar').fadeIn();
+        } else {
+          $('.topbar').fadeOut();
+        }
+        lastScrollTop = st;
+  })
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
